2f5b5875e52046e9135ba3144a14b786a90da4c4
Randallonius <jonathanprandall@gmail.com>
2019-06-16 15:46:43 -0700

    listing image link

--- src/components/Listing/ListItem.jsx
+++ src/components/Listing/ListItem.jsx
@@ -36,6 +36,45 @@ const Item = styled.div`
   }
 `
 
+const ListingImageContainer = styled.div`
+  position: relative;
+  text-align: center;
+  overflow: hidden;
+
+  * {
+    transition: all .25s ease-in-out;
+  }
+
+  &:hover {
+    .gatsby-image-wrapper {
+      opacity: 0.9;
+      filter: grayscale(100%);
+    }
+    .image-text {
+      transform: translate(-50%, -50%) scale(1);
+      transition: transform 300ms 100ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+  }
+`
+
+const ListingImageText = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  background: ${props => props.theme.colors.white};
+  border: 1px solid ${props => props.theme.colors.primary}; 
+  transform: translate(-50%, -50%) scale(0);
+  transition: transform 300ms 0ms cubic-bezier(0.6, -0.28, 0.735, 0.045);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  span {
+    padding: 15px;
+    font-style: initial;
+  }
+`
+
 const ListingText = styled.div`
   background-color: ${props => props.theme.colors.white};
 `
@@ -109,9 +148,14 @@ export default class ListItem extends Component {
     const listSize = location.pathname === ('/') ? 'masonry' : 'full';
     return (
       <Item className={listSize}>
-        <div>
-          <HeroImageSliceZone allHeroImageSlices={node.data.body} />
-        </div>
+        <ListingImageContainer>
+          <Link to={node.uid}>
+            <HeroImageSliceZone allHeroImageSlices={node.data.body} />
+            <ListingImageText className="image-text">
+              <span>read more</span>
+            </ListingImageText>
+          </Link>
+        </ListingImageContainer>
         <ListingText>
           <ListingTextInner>
             <ListingTitle>
